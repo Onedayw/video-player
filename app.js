d1040b523bc68b8e144ba9a35f663871553809f3
Nate Wang <ytowang@ucdavis.edu>
2022-11-08 02:21:58 -0800

    nit changes

--- app.js
+++ app.js
@@ -37,101 +37,100 @@ app.use(cors());
 // }]
 //You can use this to check if your server is working
 app.get('/', (req, res) => {
-	fs.readdir(directoryPath, function(err, files) {
-		let data = [];
+  fs.readdir(directoryPath, function(err, files) {
+    let data = [];
 
-		if (err) {
-			res.writeHead(404);
-			res.write('File not found');
-			return console.log('Unable to scan directory: ' + err);
-		}
-		else {
-			files.forEach(function(file) {
-				if (file.endsWith(".mp4")) {
-					const strings = file.split("|");
-					data.push({
-						timestamp: strings[0],
-						title: strings[1].slice(0, -4),
-						filename: file
-					});
-				}
-			})
-		}
+    if (err) {
+      res.writeHead(404);
+      res.write('File not found');
+      return console.log('Unable to scan directory: ' + err);
+    } else {
+      files.forEach(function(file) {
+        if (file.endsWith(".mp4")) {
+          const strings = file.split("|");
+          data.push({
+            timestamp: strings[0],
+            title: strings[1].slice(0, -4),
+            filename: file
+          });
+        }
+      })
+    }
 
-		// Render HTML with data
-		res.render('upload', {
-			data: data
-		});
-	});
+    // Render HTML with data
+    res.render('upload', {
+      data: data
+    });
+  });
 });
 
 var storage = multer.diskStorage({
-	destination: function(req, file, cb) {
-		// Uploads is the Upload_folder_name
-		cb(null, directoryPath);
-	},
-	filename: function (req, file, cb) {
-		cb(null, Date.now() + '|' + req.body.video_title + '.mp4');
-	}
+  destination: function(req, file, cb) {
+    // Uploads is the Upload_folder_name
+    cb(null, directoryPath);
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '|' + req.body.video_title + '.mp4');
+  }
 });
 
 const maxSize = 20 * 1000 * 1000;
 
 var upload = multer({
-	storage: storage,
-	limits: { fileSize: maxSize },
-	fileFilter: function (req, file, cb){
-		// Set the filetypes, it is optional
-		var filetypes = /mp4/;
-		var mimetype = filetypes.test(file.mimetype);
+  storage: storage,
+  limits: {
+    fileSize: maxSize
+  },
+  fileFilter: function(req, file, cb) {
+    // Set the filetypes, it is optional
+    var filetypes = /mp4/;
+    var mimetype = filetypes.test(file.mimetype);
 
-		var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
-		if (mimetype || extname) {
-			return cb(null, true);
-		}
+    if (mimetype || extname) {
+      return cb(null, true);
+    }
 
-		cb('File upload only supports the following filetypes - ' + filetypes, false);
-	}
+    cb('File upload only supports the following filetypes - ' + filetypes, false);
+  }
 }).single('uploaded_file');
 
 app.post('/fileUpload', function(req, res, next) {
-	// Error MiddleWare for multer file upload, so if any
-	// error occurs, the image would not be uploaded!
-	upload(req, res, function(err) {
-		if (err instanceof multer.MulterError) {
-			// ERROR occurred (here it can be occurred due
-			// to uploading image of size greater than
-			// 1MB or uploading different file type)
-			res.send(err);
-		}
-		else if (err) {
-			next(err);
-		}
-		else {
-			// SUCCESS, video successfully uploaded
-			res.send('Success, video uploaded!');
-		}
-	})
+  // Error MiddleWare for multer file upload, so if any
+  // error occurs, the image would not be uploaded!
+  upload(req, res, function(err) {
+    if (err instanceof multer.MulterError) {
+      // ERROR occurred (here it can be occurred due
+      // to uploading image of size greater than
+      // 1MB or uploading different file type)
+      res.send(err);
+    } else if (err) {
+      next(err);
+    } else {
+      // SUCCESS, video successfully uploaded
+      res.send('Success, video uploaded!');
+    }
+  })
 });
 
-app.post('/fileDelete', multer().none(), function (req, res, next) {
-	let resultHandler = function (err) {
+app.post('/fileDelete', multer().none(), function(req, res, next) {
+  let resultHandler = function(err) {
     if (err) {
       console.log('unlink failed', err);
     } else {
       console.log('file deleted');
     }
   };
-	let filepath = directoryPath + req.body.delete_filename;
+  let filepath = directoryPath + req.body.delete_filename;
 
   fs.unlink(filepath, resultHandler);
 });
 
 app.listen(port, function(error) {
-	if (error) {
-		console.log('Something went wrong', error);
-	} else {
-		console.log('Server is listening on port ' + port);
-	}
+  if (error) {
+    console.log('Something went wrong', error);
+  } else {
+    console.log('Server is listening on port ' + port);
+  }
 });
--- views/js/script.js
+++ views/js/script.js
@@ -1,31 +1,30 @@
 let videoList = document.querySelectorAll('.video-list-container .list');
 
-videoList.forEach(vid =>{
-   vid.onclick = () =>{
-      videoList.forEach(remove => {
-        remove.classList.remove('active');
-        remove.querySelector('.delete-button').removeAttribute('hidden');
-      });
-      vid.classList.add('active');
-      let src = vid.querySelector('.list-video').src;
-      let title = vid.querySelector('.list-title').innerHTML;
-      vid.querySelector('.delete-form .delete-button').setAttribute('hidden', true);
-      document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
-      document.querySelector('.main-video-container .main-video').src = src;
-      document.querySelector('.main-video-container .main-video').play();
+videoList.forEach(vid => {
+  vid.onclick = () => {
+    videoList.forEach(remove => {
+      remove.classList.remove('active');
+      remove.querySelector('.delete-button').removeAttribute('hidden');
+    });
+    vid.classList.add('active');
+    let src = vid.querySelector('.list-video').src;
+    let title = vid.querySelector('.list-title').innerHTML;
+    vid.querySelector('.delete-form .delete-button').setAttribute('hidden', true);
+    document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
+    document.querySelector('.main-video-container .main-video').src = src;
+    document.querySelector('.main-video-container .main-video').play();
 
-      let selectedButton = document.querySelector('.selected-button');
-      selectedButton.classList.add('unselected-button');
-      selectedButton.classList.remove('selected-button');
+    let selectedButton = document.querySelector('.selected-button');
+    selectedButton.classList.add('unselected-button');
+    selectedButton.classList.remove('selected-button');
 
-      let defaultButton = document.querySelector('#default-button');
-      defaultButton.classList.add('selected-button');
-      defaultButton.classList.remove('unselected-button');
-   };
+    let defaultButton = document.querySelector('#default-button');
+    defaultButton.classList.add('selected-button');
+    defaultButton.classList.remove('unselected-button');
+  };
 });
 
-function setPlaySpeed(element, x)
-{
+function setPlaySpeed(element, x) {
   document.querySelector('.main-video-container .main-video').playbackRate = x;
   let selected = document.querySelector('.selected-button');
   selected.classList.add('unselected-button');
@@ -38,6 +37,6 @@ function setPlaySpeed(element, x)
 const actualBtn = document.getElementById('actual-btn');
 const fileChosen = document.getElementById('file-chosen');
 
-actualBtn.addEventListener('change', function(){
+actualBtn.addEventListener('change', function() {
   fileChosen.textContent = this.files[0].name
 });
