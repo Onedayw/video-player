ef4501c191897ac5bbd38e20fed2a3c6f9c486d7
Nate Wang <ytowang@ucdavis.edu>
2022-11-08 02:01:56 -0800

    Add frontend and backend

---
+++ views/js/script.js
@@ -0,0 +1,43 @@
+let videoList = document.querySelectorAll('.video-list-container .list');
+
+videoList.forEach(vid =>{
+   vid.onclick = () =>{
+      videoList.forEach(remove => {
+        remove.classList.remove('active');
+        remove.querySelector('.delete-button').removeAttribute('hidden');
+      });
+      vid.classList.add('active');
+      let src = vid.querySelector('.list-video').src;
+      let title = vid.querySelector('.list-title').innerHTML;
+      vid.querySelector('.delete-form .delete-button').setAttribute('hidden', true);
+      document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
+      document.querySelector('.main-video-container .main-video').src = src;
+      document.querySelector('.main-video-container .main-video').play();
+
+      let selectedButton = document.querySelector('.selected-button');
+      selectedButton.classList.add('unselected-button');
+      selectedButton.classList.remove('selected-button');
+
+      let defaultButton = document.querySelector('#default-button');
+      defaultButton.classList.add('selected-button');
+      defaultButton.classList.remove('unselected-button');
+   };
+});
+
+function setPlaySpeed(element, x)
+{
+  document.querySelector('.main-video-container .main-video').playbackRate = x;
+  let selected = document.querySelector('.selected-button');
+  selected.classList.add('unselected-button');
+  selected.classList.remove('selected-button');
+
+  element.classList.add('selected-button');
+  element.classList.remove('unselected-button');
+}
+
+const actualBtn = document.getElementById('actual-btn');
+const fileChosen = document.getElementById('file-chosen');
+
+actualBtn.addEventListener('change', function(){
+  fileChosen.textContent = this.files[0].name
+});
